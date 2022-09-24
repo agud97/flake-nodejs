512f6fa0520df79559c9e47c063c2eb03170fc62
tparfenov <tparfenov@rbc.ru>
2022-09-24 20:13:00 +0300

    files added

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# flake-nodejs
+# sl-web-config
+
+Deployment your application using nginx inside of a docker container
+
+https://cli.vuejs.org/ru/guide/deployment.html#docker-nginx
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- flake.nix
+++ flake.nix
@@ -1,31 +1,24 @@
 {
+  description = "Sample Nix ts-node build";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dream2nix.url = "github:nix-community/dream2nix";
   };
- 
-  outputs = { self, nixpkgs }: {
-
-    nixosConfigurations = let
-      system = "x86_64-linux";
-      lib = nixpkgs.lib;
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-     in {
-         exampleIso = nixpkgs.lib.nixosSystem {
-         inherit system;
-         modules =  [
-           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-         ];
-      };
-        mysystem = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-                      ./configuration.nix
-           ];
-        }; 
-     };
-   };
+  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
+   #let
+    #dream2nixOutputs = 
+    dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
+      config.projectRoot = ./.;
+      source = gitignore.lib.gitignoreSource ./.;
+   # app = self.packages.x86_64-linux.sl-web-config;
+    };
+    #app = self.packages.x86_64-linux.sl-web-config;
+  #in nixpkgs.lib.recursiveUpdate dream2nixOutputs; 
 }
 
